1816976b9bd3b3292b60c6d1d8f8373cd78f6f9e
aonnapaht <napaht_d@cmu.ac.th>
2022-12-09 17:44:27 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
+int i=0;
+int x=1;
+int N=0;
 
 int main(){
+    while (x!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin >> x;
+    x=x;
+    
+    
+    
+    if(x%2==0){
+    i+=1;
+    }
+    else{    
+    N+=1;
+    }        
+}
+    cout << "#Even numbers = " << i-1 << "\n";
+    cout << "#Odd numbers = " << N << "\n";
+   return 0;
 }
